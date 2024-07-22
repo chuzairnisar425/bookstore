42027bf6d45094b61c8267cbd04b91c4eae9ca69
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-07-22 11:28:43 +0500

    modified

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import banner from '../../public/Banner.jpg'
 const Banner = () => {
   return (
     <>
-     <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row'>
+     <div className='my-10 max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row'>
 <div className='w-full md:w-1/2 md:mt-32 mt-12 md:order-1 order-2'>
 <div className='space-y-12'>
 <h1 className='text-4xl font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque <span className='text-blue-500'>dolor laboriosam enim.</span></h1>
