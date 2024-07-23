4d4e5b14070c79a7e9543e8c6965f04f8619a907
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-07-23 15:07:12 +0500

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Home from './Home/Home';
+import Home from './home/Home';
 import Course from './components/Course';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course" element={<Course />} />
       </Routes>
-    </BrowserRouter>
+   
   );
 }
 
--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import Courses from '../courses/Courses';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Cards from './Cards';
+import list from '../../public/list.json'
 
-const Course = () => {
-  return (
-    <div>
-      <Courses/>
-    </div>
-  );
+const Courses = () => {
+    return (
+        <>
+<Navbar/>
+<div className='min-h-screen my-10 max-w-screen-2xl container mx-auto md:px-20 px-4'>
+  <div className='mt-28 items-center justify-center text-center '>
+    <h1 className='text-2xl font-semibold md:text--4xl  '>We're delighted to have you <span className='text-blue-500'> here! :)</span> </h1>
+    <p className='mt-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium architecto, optio doloremque assumenda ex repudiandae consequatur possimus! Hic totam minima dolorem voluptates delectus corporis odio placeat tempora culpa eos.</p>
+    <button className='bg-blue-500 text-white px-4  py-2 rounded-md hover:bg-blue-700 duration-300 mt-5'> Back</button>
+  </div>
+<div className='mt-12 grid grid-cols-1 md:grid-cols-4'>
+  {
+   list.map ((item)=>(
+    <Cards key={item.id} item={item}/>
+   ))
+  }
+</div>
+</div>
+
+       <Footer/> 
+        </>
+    );
 }
 
-export default Course;
+export default Courses;
